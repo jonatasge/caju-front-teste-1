1ce7c55af1dba2e5b84e70e701fe0b5fe10d9868
Jonatas Evangelista <jonatasge@gmail.com>
2024-10-27 11:23:16 -0300

    refactor: routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,24 +1,25 @@
 import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
+
 import routes from "./routes";
-import DashboardPage from "@/pages/Dashboard";
-import NewUserPage from "@/pages/NewUser";
 
 const Router = () => {
   return (
     <div style={{ marginTop: 64 }}>
       <HashRouter>
         <Switch>
-          <Route exact path={routes.dashboard} component={DashboardPage} />
-          <Route exact path={routes.newUser} component={NewUserPage} />
-          <Route
-            exact
-            path={routes.history}
-            component={() => <div>History</div>}
-          />
+          {Object.values(routes)
+            .filter((r) => !r.redirectTo)
+            .map((route) => (
+              <Route key={route.path} {...route} />
+            ))}
 
-          <Route exact path="*">
-            <Redirect to={routes.dashboard} />
-          </Route>
+          {Object.values(routes)
+            .filter((r) => r.redirectTo)
+            .map(({ redirectTo, ...route }) => (
+              <Route key={route.path} {...route}>
+                <Redirect to={redirectTo as string} />
+              </Route>
+            ))}
         </Switch>
       </HashRouter>
     </div>
