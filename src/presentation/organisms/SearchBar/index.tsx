7aa327738773bc71ea9eef6e5046f2628542703a
Jonatas Evangelista <jonatasge@gmail.com>
2024-10-28 23:11:03 -0300

    feat: adds the mask to the search field

--- src/presentation/organisms/SearchBar/index.tsx
+++ src/presentation/organisms/SearchBar/index.tsx
@@ -4,6 +4,8 @@ import Button from "@/presentation/atoms/Button";
 import IconButton from "@/presentation/atoms/IconButton";
 import TextField from "@/presentation/molecules/TextField";
 import * as S from "./styles";
+import { maskCpf } from "@/validation/masks/cpf";
+import { validateCpf } from "@/validation/cpf";
 
 type Props = React.ComponentProps<typeof S.Container> & {
   onAdd(): void;
@@ -13,13 +15,33 @@ type Props = React.ComponentProps<typeof S.Container> & {
 
 const SearchBar = ({ onAdd, onRefresh, onSearch, ...props }: Props) => {
   function onChange(event: React.KeyboardEvent) {
-    const input = event.target as HTMLInputElement;
-    onSearch?.(input.value);
+    const input = event.target as HTMLInputElement
+    maskCpf(input);
+    const cpfOnlyNumbers = input.value.replace(/\D/g, '');
+    const isEmpty = !cpfOnlyNumbers;
+    const isValid = isEmpty || validateCpf(input.value);
+    if (isValid) onSearch?.(cpfOnlyNumbers);
+  }
+
+  function onFocus(event: React.FocusEvent) {
+    const input = event.target as HTMLInputElement
+    maskCpf(input);
+  }
+
+  function onBlur(event: React.FocusEvent) {
+    const input = event.target as HTMLInputElement
+    const isEmpty = !input.value.replace(/\D/g, '');
+    if (isEmpty) input.value = '';
   }
 
   return (
     <S.Container {...props}>
-      <TextField placeholder="Digite um CPF válido" onChange={onChange} />
+      <TextField
+        placeholder="Digite um CPF válido"
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
 
       <S.Actions>
         <IconButton
